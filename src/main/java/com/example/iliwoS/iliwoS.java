80cff0a6346c125846907e97cecbee6c9dffee10
Роман Таймасов <soulinfect@mail.ru>
2025-04-03 10:46:53 +0500

    Обновил код: исправил ошибки и добавил обработку чата

--- src/main/java/com/example/iliwoS/iliwoS.java
+++ src/main/java/com/example/iliwoS/iliwoS.java
@@ -1,6 +1,7 @@
 package com.example.examplemod;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
@@ -8,20 +9,23 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = SowlAddon.MODID, version = SowlAddon.VERSION) 
-public class SowlAddon {  
-    public static final String MODID = "sowladdon";  
+@Mod(modid = iliwoS.MODID, version = iliwoS.VERSION, acceptedMinecraftVersions = "[1.8.9]")
+public class iliwoS {
+    public static final String MODID = "iliwoS";
     public static final String VERSION = "1.0";
 
-    @EventHandler
+    // String apiKey = configLoader.getApiKey();
+    @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        MinecraftForge.EVENT_BUS.register(this);  // Регистрация событий
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
     @SubscribeEvent
     public void onChat(ClientChatReceivedEvent event) {
-        if (event.message.getUnformattedText().contains("ping")) {
-            Minecraft.getMinecraft().thePlayer.sendChatMessage("pong");
+        String msg = event.message.getUnformattedText();
+        if (msg.equalsIgnoreCase("/uuid")) {
+            Minecraft.getMinecraft().thePlayer.addChatMessage(
+                    new ChatComponentText(Minecraft.getMinecraft().thePlayer.getName()));
         }
     }
 }
